c19a2951449e1d6dfc6040bb30ceacb653fa1ff9
drinkcoffee101 <dcoffie92@gmail.com>
2019-12-31 13:48:31 -0800

    Updates

---
+++ precache-manifest.67ff60156667ea8679408b767b816e65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af76862454671c96d760f405d14ea3e0",
+    "url": "/react_monsters_rolodex/index.html"
+  },
+  {
+    "revision": "6e643dffd71df19bedbb",
+    "url": "/react_monsters_rolodex/static/css/main.a79866db.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/react_monsters_rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_monsters_rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e643dffd71df19bedbb",
+    "url": "/react_monsters_rolodex/static/js/main.a5e19ca7.chunk.js"
+  },
+  {
+    "revision": "f21a16338e8706e123a3",
+    "url": "/react_monsters_rolodex/static/js/runtime-main.07158689.js"
+  }
+]);
